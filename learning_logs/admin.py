d0295e75f725c46a4dff3edb7713b868e73e7cad
gabrielcorcelli <gabrielcorcelli10@gmail.com>
2025-05-14 18:44:33 -0300

    Refazendo para relembrar

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -4,6 +4,8 @@ from learning_logs.models import Topic,Entry
 
 
 admin.site.register(Topic)
-admin.site.register(Entry)
+@admin.register(Entry)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["text","topic"]
 
 # Register your models here.
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,21 +2,18 @@ from django.shortcuts import render
 from .models import Topic
 
 # Create your views here.
-def index(request):
-    """Página principal do learning_log"""
-    return render(request, 'learning_logs/index.html')
+def index (request):
+    return render (request,'learning_logs/index.html')
 
 
-def topics(request):
-    """Mostra todos os assuntos"""
+def topics (request):
     topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics}
-    return render(request,'learning_logs/topics.html',context)
+    context = {'topics':topics}
+    return render (request,'learning_logs/topics.html', context)
 
-
-def topic(request,topic_id):
-    "Mostra todas as entradas de um tópico"
-    topic=Topic.objects.get(id=topic_id)
+def topic (request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
     entries = topic.entry_set.order_by('-date_added')
-    context={'topic' : topic, 'entries' : entries}
-    return render(request,'learning_logs/topic.html',context)
+    context = {'topic':topic,'entries':entries}
+    return render (request,'learning_logs/topic.html',context)
+    
